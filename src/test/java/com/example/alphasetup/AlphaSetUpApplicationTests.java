a50b2af484ddb8b8d1e751d8f9bbbe98faa193d3
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-07 22:37:48 +0000

    update alpha set up ....

---
+++ src/test/java/com/example/alphasetup/AlphaSetUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alphasetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaSetUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
